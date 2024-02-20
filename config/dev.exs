cb5dbe9490d527c818b646d86a42082ef87a3920
zastrixarundell <luka@bodroza.com>
2024-02-20 19:54:00 +0100

    Timestamps with logger in dev

--- config/dev.exs
+++ config/dev.exs
@@ -53,7 +53,7 @@ config :data_reeler, DataReelerWeb.Endpoint,
 config :data_reeler, dev_routes: true
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "[$time] [$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
