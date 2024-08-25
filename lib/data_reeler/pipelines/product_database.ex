596b48710c5365f0129400b85b35170dbde62403
zastrixarundell <luka@bodroza.com>
2024-08-25 07:06:44 +0200

    Break code on purpose to see the error

--- lib/data_reeler/pipelines/product_database.ex
+++ lib/data_reeler/pipelines/product_database.ex
@@ -5,11 +5,17 @@ defmodule DataReeler.Pipelines.ProductDatabase do
 
   @impl Crawly.Pipeline
   def run(item, state) do
-    case DataReeler.Stores.upsert_product_by_sku_and_provider(item) do
-      {:ok, _} ->
-        Logger.info("Saved product: #{inspect(item)}")
-      {:error, error} ->
-        Logger.warning("Falied to insert production #{inspect(item)} with error: #{inspect(error)}")
+    try do
+      case DataReeler.Stores.upsert_product_by_sku_and_provider(item) do
+        {:ok, _} ->
+          Logger.info("Saved product: #{inspect(item)}")
+        {:error, error} ->
+          Logger.warning("Falied to insert production #{inspect(item)} with error: #{inspect(error)}")
+      end
+    rescue
+      e in RuntimeError -> e
+        Logger.error("Failed to save #{item}")
+        rairse "nope"
     end
 
     {item, state}
