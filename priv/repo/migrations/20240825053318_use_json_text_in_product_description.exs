25e1714ac02cfe40efc50701db391c6e0d54adb5
zastrixarundell <luka@bodroza.com>
2024-08-26 01:28:14 +0200

    Add up/down migration for data

--- priv/repo/migrations/20240825053318_use_json_text_in_product_description.exs
+++ priv/repo/migrations/20240825053318_use_json_text_in_product_description.exs
@@ -1,9 +1,15 @@
 defmodule DataReeler.Repo.Migrations.UseJsonTextInProductDescription do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:products) do
       modify :description, {:array, :text}, null: false
+    end
+  end
+  
+  def down do
+    alter table(:products) do
+      modify :description, {:array, :string}, null: false
     end
   end
 end
